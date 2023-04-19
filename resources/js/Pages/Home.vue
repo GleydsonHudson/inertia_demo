0c815ffe0fde9bddfdffcc17ec55027078cb96a2
Gleydson Silva <ghudson.ti@gmail.com>
2023-04-19 14:40:08 +0200

    Learn about Pages.

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>Hello, {{ name }}</h1>
+
+    <p>In the series, we will use the following frameworks:</p>
+
+    <ul>
+        <li v-for="framework of frameworks" v-text="framework"></li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: {
+        name: String,
+        frameworks: Array
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
