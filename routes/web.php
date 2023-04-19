8951644c9bec4df53974caff2911946f4faf9a26
Gleydson Silva <ghudson.ti@gmail.com>
2023-04-19 14:56:01 +0200

    Learn about Inertia Links

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return inertia('Home', [
-        'name' => 'hudson',
-        'frameworks' => ['Laravel', 'Vue', 'Inertia']
-    ]);
+Route::get('/', static function () {
+    return inertia('Home');
+});
+
+Route::get('/users', static function () {
+    return inertia('Users');
+});
+
+Route::get('/settings', static function () {
+    return inertia('Settings');
 });
