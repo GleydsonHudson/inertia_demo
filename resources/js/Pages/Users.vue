46e6b57c149c8f02cbcf521a11aaa9345ad2d7cb
Gleydson Silva <ghudson.ti@gmail.com>
2023-04-23 17:00:10 +0200

    Add the Layout to the components.

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,21 +1,21 @@
 <template>
-    <h1>Users</h1>
+    <Layout>
+        <h1 class="text-3xl">Users</h1>
 
-    <Nav/>
+        <div style="margin-top: 900px;">
+            <p>The current time is {{ time }}.</p>
 
-    <div style="margin-top: 900px;">
-        <p>The current time is {{ time }}.</p>
-
-        <Link href="/users" class="text-blue-500" preserve-scroll>Refresh</Link>
-    </div>
+            <Link href="/users" class="text-blue-500" preserve-scroll>Refresh</Link>
+        </div>
+    </Layout>
 </template>
 
 <script>
-import Nav from "../Shared/Nav.vue";
 import {Link} from "@inertiajs/vue3";
+import Layout from "../Shared/Layout.vue";
 
 export default {
-    components: {Nav, Link},
+    components: {Layout, Link},
 
     props: {time: String}
 
