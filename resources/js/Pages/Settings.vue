f71adee8fc904bcc2c1e55d9887e9b90d3b53654
Gleydson Silva <ghudson.ti@gmail.com>
2023-04-24 01:10:22 +0200

    Make the persistent layout.

--- resources/js/Pages/Settings.vue
+++ resources/js/Pages/Settings.vue
@@ -1,7 +1,7 @@
 <template>
-    <Layout>
-        <h1 class="text-3xl">Settings</h1>
-    </Layout>
+    <h1 class="text-3xl">
+        Settings
+    </h1>
 
 </template>
 
@@ -9,7 +9,7 @@
 import Layout from "../Shared/Layout.vue";
 
 export default {
-    components: {Layout}
+    layout: Layout
 
 }
 </script>
--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,13 +1,11 @@
 <template>
-    <Layout>
-        <h1 class="text-3xl">Users</h1>
+    <h1 class="text-3xl">Users</h1>
 
-        <div style="margin-top: 900px;">
-            <p>The current time is {{ time }}.</p>
+    <div style="margin-top: 900px;">
+        <p>The current time is {{ time }}.</p>
 
-            <Link href="/users" class="text-blue-500" preserve-scroll>Refresh</Link>
-        </div>
-    </Layout>
+        <Link href="/users" class="text-blue-500" preserve-scroll>Refresh</Link>
+    </div>
 </template>
 
 <script>
@@ -15,7 +13,8 @@ import {Link} from "@inertiajs/vue3";
 import Layout from "../Shared/Layout.vue";
 
 export default {
-    components: {Layout, Link},
+    layout: Layout,
+    components: {Link},
 
     props: {time: String}
 
