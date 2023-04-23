a1b1f438b8f89a55ccecd3da1a90dab8c975bf13
Gleydson Silva <ghudson.ti@gmail.com>
2023-04-24 01:02:18 +0200

    Setting up share info across all components.

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -1,7 +1,14 @@
 <template>
     <section class="p-6 bg-gray-200">
         <header class="flex justify-between">
-            <h1 class="font-bold text-lg">My app</h1>
+            <div class="flex items-center space-x-2">
+                <h1 class="font-bold text-lg">My app</h1>
+
+                <p>
+                    Welcome Back, {{ username }}
+                </p>
+            </div>
+
             <Nav/>
         </header>
     </section>
@@ -19,7 +26,13 @@ import Nav from "./Nav.vue";
 
 export default {
     name: "Layout",
-    components: {Nav}
+    components: {Nav},
+
+    computed: {
+        username(){
+            return this.$page.props.auth.user.username;
+        }
+    }
 }
 </script>
 
