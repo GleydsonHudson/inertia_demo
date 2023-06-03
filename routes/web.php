cb9047beaba4c28d909f315145f6232a8899be68
Gleydson Silva <ghudson.ti@gmail.com>
2023-06-03 11:46:41 +0200

    Style changes by pint command.

--- routes/web.php
+++ routes/web.php
@@ -2,24 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', static function () {
     return inertia('Home');
 });
 
 Route::get('/users', static function () {
-    return inertia('Users',[
-        'time' => now()->toTimeString()
+    return inertia('Users', [
+        'time' => now()->toTimeString(),
     ]);
 });
 
