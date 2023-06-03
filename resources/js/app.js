bb431edf127e9179500b6a0a532d9fd5ae180d27
Gleydson Silva <ghudson.ti@gmail.com>
2023-06-03 11:46:10 +0200

    Update the app.js to reflect changes in the Inertia framework.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,21 @@
 import {createApp, h} from 'vue';
 import {createInertiaApp} from '@inertiajs/vue3';
-import {InertiaProgress} from "@inertiajs/progress";
 import Layout from "./Shared/Layout.vue";
+import {resolvePageComponent} from "laravel-vite-plugin/inertia-helpers";
 
-InertiaProgress.init();
 
 createInertiaApp({
-    resolve: (name) => {
-        const pages = import.meta.glob('./Pages/**/*.vue', {eager: true})
-        let page = pages['./Pages/${name}.vue']
-        page.default.layout = page.default.layout || Layout
-        return page
+    resolve: async name => {
+        const page = await resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob("./Pages/**/*.vue"));
+        page.default.layout ??= Layout;
+        return page;
     },
+    // resolve: (name) => {
+    //     const pages = import.meta.glob('./Pages/**/*.vue', {eager: true})
+    //     let page = pages['./Pages/${name}.vue']
+    //     page.default.layout = page.default.layout || Layout
+    //     return page
+    // },
     setup({el, App, props, plugin}) {
         return createApp({render: () => h(App, props)})
             .use(plugin)
